4a7ed07182a5fff5f7b76e7f5663227da460bd86
Rafael Arcanjo de Oliveira <rafael9559@hotmail.com>
2020-10-30 00:26:48 -0300

    AC4 - Linguagem de Programação 2

--- ac4.py
+++ ac4.py
@@ -9,7 +9,8 @@ class Medico:
 
     def informacoes_medico(self):
         print('-'*30)
-        print('\n Dados Médico: ')
+        print('\n Dados do Médico: ')
+        print ('')
         print('Nome: ', self.nome)
         print('CRM: ', self.CRM)
 
@@ -18,7 +19,18 @@ class Medico:
 medico = Medico('Rafael Arcanjo de Oliveira', 1903205, 42439207888, 433768575, '(11) 98736-8374', 10000)
 medico.informacoes_medico()
 
-class Paciente:
+class Quarto:
+    def __init__(self, numero, andar):
+        self.numero = numero
+        self.andar = andar
+
+    def informacoes_paciente(self):
+        print('-'*30)
+        print('\n Dados do Quarto Paciente: ')
+        print('Numero: ', self.numero)
+        print('Andar: ', self.andar)
+
+class Paciente():
     def __init__(self, nome, RG, CPF, endereco, telefone, datanascimento):
         self.nome = nome
         self.RG = RG
@@ -27,4 +39,18 @@ class Paciente:
         self.telefone = telefone
         self.datanascimento = datanascimento
 
+    def informacoes_paciente(self):
+        print('-'*30)
+        print('\n Dados do Paciente: ')
+        print ('')
+        print('Nome: ', self.nome)
+        print('CPF: ', self.CPF)
+        print('Data de Nascimento: ', self.datanascimento)
+
+
+quarto = Quarto(10, '1 andar')
+quarto.informacoes_paciente()
+
+paciente = Paciente('Joãozinho', 43434343, 99999999999, 'Rua das Lagrimas, 123', '(11) 99999999', '19/02/1999')
+paciente.informacoes_paciente()
 
